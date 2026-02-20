8d2a539d5a279e027ab8756a5793845671c00bc2
Chavisky D <auukunn.bkk@gmail.com>
2026-02-20 15:57:12 +0700

    changing browserRouter to hashrouter to fix CORS

--- apps/admin-web/src/main.jsx
+++ apps/admin-web/src/main.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import { BrowserRouter } from "react-router-dom";
+import { HashRouter } from "react-router-dom";
 import App from "./App";
 import { AuthProvider } from "./context/AuthContext";
 import { UiProvider } from "./context/UiContext";
@@ -8,12 +8,12 @@ import "./styles.css";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <BrowserRouter basename={import.meta.env.BASE_URL}>
+    <HashRouter>
       <UiProvider>
         <AuthProvider>
           <App />
         </AuthProvider>
       </UiProvider>
-    </BrowserRouter>
+    </HashRouter>
   </React.StrictMode>,
 );
